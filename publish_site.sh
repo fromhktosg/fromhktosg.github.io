12f5f637dda2b5d5dd1fb7b3a4ff0a0728d7e773
David Li <DavidLi@Davids-MacBook-Pro.local>
2023-12-09 15:36:05 +0800

    Updated site version at 2023-12-09 15:36:02

--- publish_site.sh
+++ publish_site.sh
@@ -10,25 +10,22 @@ echo "Syncing latest code from source and master..."
 
 echo "Switching to source branch to make changes..."
 git checkout source
-git add --all
+# git add --all
+git add .
 git commit -m "Changes made at $TIMESTAMP"
-git push origin source
 
-# echo "Building the website..."
-# gem uninstall google-protobuf -a; BUNDLE_FORCE_RUBY_PLATFORM=true bundle install    # https://github.com/protocolbuffers/protobuf/issues/7070#issuecomment-1370248311
-bundle config set force_ruby_platform true
-bundle install
+echo "Building the website..."
 bundle exec jekyll build
 
-# echo "Switching to master branch to publish the updated site..."
+echo "Switching to master branch to publish the updated site..."
 git checkout master
 
-# # Optionally, uncomment the following line to reset if necessary
-# # ls | grep -v '^_site$' | xargs rm -r
+# Optionally, uncomment the following line to reset if necessary
+# ls | grep -v '^_site$' | xargs rm -r
 
 cp -r _site/* .
 git add .
 git commit -m "Updated site version at $TIMESTAMP"
 git push origin master
-# git push origin source
+git push origin source
 git checkout source
