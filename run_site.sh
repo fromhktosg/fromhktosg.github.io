0d0b3782403a8210f72bf27f2c574b375569ed67
David Li <DavidLi@Davids-MacBook-Pro.local>
2023-12-17 12:17:10 +0800

    Updated site version at 2023-12-17 12:17:04

--- run_site.sh
+++ run_site.sh
@@ -6,4 +6,4 @@ TIMESTAMP=$(date +"%Y-%m-%d %T")
 # Function to serve and test locally
 echo "Switching to source branch and starting Jekyll..."
 git checkout source
-bundle exec jekyll serve --watch --port 4001
+jekyll serve --watch --port 4001 --trace
