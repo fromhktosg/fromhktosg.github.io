81934fadb3c74cc53d7179f75d8a78f90950ac3a
jaxball <uscjlin@gmail.com>
2024-12-19 19:32:01 -0800

    Updated site version at 2024-12-19 19:31:54

--- run_site.sh
+++ run_site.sh
@@ -4,6 +4,7 @@
 TIMESTAMP=$(date +"%Y-%m-%d %T")
 
 # Function to serve and test locally
+bundle install
 echo "Switching to source branch and starting Jekyll..."
 git checkout source
-jekyll serve --watch --port 4001
+jekyll serve --watch --port 4001 --trace
